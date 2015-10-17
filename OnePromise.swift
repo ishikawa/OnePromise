a649cb324ddb4dafdcc27fb67688e4f8157f99ff
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2015-10-17 19:58:21 +0900

    thread safety

--- OnePromise.swift
+++ OnePromise.swift
@@ -20,10 +20,41 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-public enum PromiseState<T> {
+public enum PromiseState<T>: CustomStringConvertible {
     case Pending
     case Fulfilled(T)
     case Rejected(NSError)
+
+    public var description: String {
+        switch self {
+        case .Pending:
+            return "Pending"
+        case .Fulfilled(_):
+            return "Fulfilled"
+        case .Rejected(_):
+            return "Rejected"
+        }
+    }
+}
+
+// For thread safety
+private var syncQueueTag = 0xfa9
+
+private let syncQueue: dispatch_queue_t = {
+    let q = dispatch_queue_create("jp.ko9.OnePromise.sync", DISPATCH_QUEUE_SERIAL)
+
+    dispatch_queue_set_specific(q, &syncQueueTag, &syncQueueTag, nil)
+
+    return q
+}()
+
+private func performSync(block: () -> Void) {
+    if dispatch_get_specific(&syncQueueTag) == nil {
+        dispatch_sync(syncQueue, block)
+    }
+    else {
+        block()
+    }
 }
 
 public class Promise<T> {
@@ -40,73 +71,121 @@ public class Promise<T> {
 
     }
 
-    public func then<U>(onFulfilled: (ValueType) -> Promise<U>, _ onRejected: ((NSError) -> Void)? = nil) -> Promise<U>
-    {
-        return Promise<U>({ (nextPromise) -> Void in
-            self.appendOnFulfilled(nextPromise, onFulfilled)
+    public func then<U>(onFulfilled: ValueType -> Promise<U>, _ onRejected: (NSError -> Void)? = nil) -> Promise<U> {
+        let nextPromise = Promise<U>()
 
-            if let onRejected = onRejected {
-                self.appendOnRejected(nextPromise, onRejected)
-            }
-        })
+        performSync {
+            self.appendOnFulfilled(nextPromise, onFulfilled)
+            self.appendOnRejected(nextPromise, onRejected)
+        }
+
+        return nextPromise
     }
 
-    public func then<U>(onFulfilled: (ValueType) -> U, _ onRejected: ((NSError) -> Void)? = nil) -> Promise<U>
-    {
-        return Promise<U>({ (nextPromise) -> Void in
-            self.appendOnFulfilled(nextPromise, onFulfilled)
+    public func then<U>(onFulfilled: ValueType -> U, _ onRejected: (NSError -> Void)? = nil) -> Promise<U> {
+        let nextPromise = Promise<U>()
 
-            if let onRejected = onRejected {
-                self.appendOnRejected(nextPromise, onRejected)
-            }
-        })
+        performSync {
+            self.appendOnFulfilled(nextPromise, onFulfilled)
+            self.appendOnRejected(nextPromise, onRejected)
+        }
+
+        return nextPromise
     }
 
-    public func then(onFulfilled: ((ValueType) -> T)?, _ onRejected: ((NSError) -> Void)? = nil) -> Promise<T>
-    {
-        return Promise<T>({ (nextPromise) -> Void in
-            if let onFulfilled = onFulfilled {
-                self.appendOnFulfilled(nextPromise, onFulfilled)
-            }
+    public func then(onFulfilled: (ValueType -> T)?, _ onRejected: (NSError -> Void)? = nil) -> Promise<T> {
+        let nextPromise = Promise<T>()
 
-            if let onRejected = onRejected {
-                self.appendOnRejected(nextPromise, onRejected)
-            }
-        })
+        performSync {
+            self.appendOnFulfilled(nextPromise, onFulfilled)
+            self.appendOnRejected(nextPromise, onRejected)
+        }
+
+        return nextPromise
     }
 
     private func appendOnFulfilled<U>(nextPromise: Promise<U>, _ onFulfilled: ValueType -> Promise<U>) {
-        self.onFulfilled.append({ (value) -> Void in
-            onFulfilled(value).then(nextPromise.fulfill)
-        })
+        switch self.state {
+        case .Pending:
+            self.onFulfilled.append({ (value) -> Void in
+                onFulfilled(value).then(nextPromise.fulfill)
+            })
+        case .Fulfilled(let value):
+            dispatch_async(dispatch_get_main_queue(), {
+                onFulfilled(value).then(nextPromise.fulfill)
+            })
+        case .Rejected(_):
+            return
+        }
     }
 
-    private func appendOnFulfilled<U>(nextPromise: Promise<U>, _ onFulfilled: ValueType -> U) {
-        self.onFulfilled.append({ (value) -> Void in
-            nextPromise.fulfill(onFulfilled(value))
-        })
+    private func appendOnFulfilled<U>(nextPromise: Promise<U>, _ onFulfilled: (ValueType -> U)?) {
+        if let onFulfilled = onFulfilled {
+            switch self.state {
+            case .Pending:
+                self.onFulfilled.append({ (value) -> Void in
+                    nextPromise.fulfill(onFulfilled(value))
+                })
+            case .Fulfilled(let value):
+                dispatch_async(dispatch_get_main_queue(), {
+                    nextPromise.fulfill(onFulfilled(value))
+                })
+            case .Rejected(_):
+                return
+            }
+        }
     }
 
-    private func appendOnRejected<U>(nextPromise: Promise<U>, _ onRejected: NSError -> Void) {
-        self.onRejected.append({ (error) -> Void in
-            onRejected(error)
-            nextPromise.reject(error)
-        })
+    private func appendOnRejected<U>(nextPromise: Promise<U>, _ onRejected: (NSError -> Void)?) {
+        if let onRejected = onRejected {
+            switch self.state {
+            case .Pending:
+                self.onRejected.append({ (error) -> Void in
+                    onRejected(error)
+                    nextPromise.reject(error)
+                })
+            case .Fulfilled(_):
+                return
+            case .Rejected(let error):
+                dispatch_async(dispatch_get_main_queue(), {
+                    onRejected(error)
+                })
+                nextPromise.reject(error)
+            }
+        }
     }
 
     public func fulfill(value: ValueType) {
-        self.state = .Fulfilled(value)
+        performSync {
+            if case .Pending = self.state {
+                self.state = .Fulfilled(value)
 
-        for cb in self.onFulfilled {
-            cb(value)
+                for cb in self.onFulfilled {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        cb(value)
+                    })
+                }
+            }
+            else {
+                fatalError("\(self.state) must not transition to any other state.")
+            }
         }
     }
 
     public func reject(error: NSError) {
-        self.state = .Rejected(error)
+        performSync {
+            if case .Pending = self.state {
+                self.state = .Rejected(error)
 
-        for cb in self.onRejected {
-            cb(error)
+                for cb in self.onRejected {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        cb(error)
+                    })
+                }
+            }
+            else {
+                fatalError("\(self.state) must not transition to any other state.")
+            }
         }
     }
 }
