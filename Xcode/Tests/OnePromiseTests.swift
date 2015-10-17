5b04bc754682071af00d1b46043266f091a3b59b
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2015-10-17 17:43:53 +0900

    1st implementation

---
+++ Xcode/Tests/OnePromiseTests.swift
@@ -0,0 +1,35 @@
+import UIKit
+import XCTest
+import OnePromise
+
+class OnePromiseTests: XCTestCase {
+
+    func createPromise() -> Promise<Int> {
+        return Promise()
+    }
+
+    func testExample() {
+        let expectation = self.expectationWithDescription("done")
+
+        let promise = Promise<Int>()
+
+        promise
+            .then({ (v) -> Int in
+                v * 2
+            })
+            .then({ (v:Int) -> Promise<Double> in
+                XCTAssertEqual(v, 2000)
+                expectation.fulfill()
+
+                return Promise()
+            })
+            .then({ (v) in
+
+            })
+
+        promise.fulfill(1000)
+
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+
+}
